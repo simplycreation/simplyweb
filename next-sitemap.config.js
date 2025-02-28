428f05d206f3d455e24761745d58d810b1bae165
Irza Orvala Alfandi <orvala9@gmail.com>
2025-02-28 22:06:44 +0700

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://simplycreation.id", // Ganti dengan URL websitemu
+  generateRobotsTxt: true, // Membuat robots.txt secara otomatis
+  changefreq: "daily", // Frekuensi perubahan (opsional)
+  priority: 0.7, // Prioritas default untuk halaman (opsional)
+  sitemapSize: 5000, // Maksimal URL per sitemap file (opsional)
+  exclude: ["/admin", "/secret-page"], // Halaman yang tidak mau dimasukkan ke sitemap (opsional)
+};
